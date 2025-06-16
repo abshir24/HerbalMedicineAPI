dd3e39706db9496a9ee43328d6a46f8c889b7161
abshir24 <abshirm@live.com>
2025-06-16 12:16:59 -0700

    added status code to health check

--- app.py
+++ app.py
@@ -444,7 +444,7 @@ def upload_dataset():
 async def health_check():
     try:
         index.describe_index_stats()  # or another lightweight call
-        return jsonify({"pinecone": "healthy"})
+        return jsonify({"pinecone": "healthy"}), 200
     except PineconeException as e:
         return jsonify({"pinecone": "unhealthy", "error": str(e)}), 500
 
